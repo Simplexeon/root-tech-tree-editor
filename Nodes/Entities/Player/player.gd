c4536a19047e702a3f90033718cd32e448d48849
Ryan Doyle (RIT Student) <rad8555@rit.edu>
2025-01-24 11:43:32 -0500

    more goofy character controller

--- Nodes/Entities/Player/player.gd
+++ Nodes/Entities/Player/player.gd
@@ -8,9 +8,12 @@ class_name Player2D
 @export var AccelerationRamp : Curve;
 @export var TargetSpeed : float;
 @export var Friction : Curve;
+@export var MaxFriction : float;
+@export_range(-1.0, 1.0) var TurningPoint : float;
 
 @export_subgroup("Controls")
 @export_range(0.05, 5.0) var MouseSensitivity : float;
+@export var MouseRange : float;
 
 
 @export_subgroup("Camera")
@@ -55,6 +58,11 @@ func _process(delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
+	
+	# Limit Crosshair
+	var mouse_vect = Crosshair.global_position - global_position;
+	Crosshair.global_position = global_position + (mouse_vect.normalized() * min(mouse_vect.length(), MouseRange));
+	
 	process_movement(delta);
 
 
@@ -72,23 +80,29 @@ func get_camera_pos(crosshair_pos : Vector2) -> Vector2:
 func process_movement(delta : float) -> void:
 	
 	var move_dir : Vector2 = Vector2.ZERO;
-	if(Input.is_action_just_pressed("up")):
+	if(Input.is_action_pressed("up")):
 		move_dir.y -= 1;
-	if(Input.is_action_just_pressed("down")):
+	if(Input.is_action_pressed("down")):
 		move_dir.y += 1;
-	if(Input.is_action_just_pressed("left")):
+	if(Input.is_action_pressed("left")):
 		move_dir.x -= 1;
-	if(Input.is_action_just_pressed("right")):
+	if(Input.is_action_pressed("right")):
 		move_dir.x += 1;
 	move_dir = move_dir.normalized();
 	
 	
-	# TODO
-	# Implement not moving
+	if(move_dir == Vector2.ZERO):
+		move_dir = (velocity * -1).normalized();
 	
-	var force : float = AccelerationRamp.sample_baked(min(velocity.length() / TargetSpeed, 1.0)) * Acceleration;
-	var turn_amount : float = Friction.sample_baked(1 - ((move_dir.dot(velocity.normalized()) + 1) / 2));
+	#var friction_amount : float = 1 - ((move_dir.dot(velocity.normalized()) + 1) / 2);
 	
-	velocity += move_dir * (force);
-	print(velocity);
+	var force : float = AccelerationRamp.sample_baked(min(velocity.length() / TargetSpeed, 1.0)) * Acceleration * delta;
+	var target_velocity : Vector2 = velocity + (move_dir * force);
+	
+	if(target_velocity.normalized().dot(velocity.normalized()) <= TurningPoint and velocity != Vector2.ZERO):
+		velocity = Vector2.ZERO;
+	else:
+		velocity = target_velocity;
+	
+	
 	move_and_slide();
